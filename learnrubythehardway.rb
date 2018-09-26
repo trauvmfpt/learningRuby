78555a4f6c810f9343061d20459e2bb0a7f8cfa6
Tra <trauvmd00621@fpt.edu.vn>
2018-09-26 22:50:50 +0700

    Had fun with ex16 and writing files

--- learnrubythehardway.rb
+++ learnrubythehardway.rb
@@ -1,16 +1,33 @@
-filename = ARGV.first
+filename = ARGV.first 
 
-txt = open(filename)
+puts "We're going to erase #{filename}."
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you DO want that, hit RETURN."
 
-puts "Here's your file #{filename}:\n\n"
-puts txt.read, "\n"
+$stdin.gets
 
-print "Type the filename again: \n"
-file_again = $stdin.gets.chomp
+puts "Opening the file..."
+target = open(filename, 'w')
 
-txt_again = open(file_again)
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
 
-puts txt_again.read, "\n"
+puts "Now I'm going to ask you for three lines."
 
-txt.close()
-txt_again.close()
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+target.write("""
+	#{line1}
+	#{line2}
+	#{line3}
+	""")
+
+puts "And finally, we close it."
+target.close()
